abd2160f9f2ff978c66c6ba99f3004df95ce548a
cotasi135 <cotasi135@gmail.com>
2024-03-22 19:07:55 +0900

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    plugins: [
-      require('postcss-nesting'), // 중첩 플러그인 추가
-      require('tailwindcss'),
-      require('autoprefixer'),
-      // 다른 PostCSS 플러그인이 있다면 여기에 추가하세요
-    ]
+    plugins: {
+        'tailwindcss/nesting': {},
+        tailwindcss: {},
+        autoprefixer: {}
+    }
   }
