411ee2388dd34c0f3445566d66d3204749608faa
cotasi135 <cotasi135@gmail.com>
2024-03-22 22:34:55 +0900

    posscssenv

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-// postcss.config.js
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': true },
-    },
-  }
-}
+  plugins: [
+      require('postcss-nesting')(),
+      require('autoprefixer')(),
+      // Add other PostCSS plugins here if needed
+  ]
+};
